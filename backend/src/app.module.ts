d465f6c49f4068d0071e7990496a15b47dafde2c
jzbm <danielgadzina@protonmail.com>
2026-01-03 11:45:48 +0100

    feat(backend): integrate all modules in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { AircraftModule } from './modules/aircraft/aircraft.module';
+import { AirportsModule } from './modules/airports/airports.module';
+import { FlightPlansModule } from './modules/flight-plans/flight-plans.module';
+import { CalculationsModule } from './modules/calculations/calculations.module';
+import { WeatherModule } from './modules/weather/weather.module';
+import { WeatherQueueModule } from './modules/weather-queue/weather-queue.module';
+import { NotamModule } from './modules/notam/notam.module';
+import { PdfModule } from './modules/pdf/pdf.module';
+import { AirspaceModule } from './modules/airspace/airspace.module';
 
 @Module({
   imports: [
@@ -8,6 +20,18 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
+    RabbitMQModule,
+    AuthModule,
+    UsersModule,
+    AircraftModule,
+    AirportsModule,
+    FlightPlansModule,
+    CalculationsModule,
+    WeatherModule,
+    WeatherQueueModule,
+    NotamModule,
+    PdfModule,
+    AirspaceModule,
   ],
   controllers: [],
   providers: [],
